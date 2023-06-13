9f489ce29a83ce5d2e879faeadaa67e1fa9ca0c5
FT-37aDanielLuevano <130702268+FT-37aDanielLuevano@users.noreply.github.com>
2023-06-13 16:23:05 -0600

    CREACION MODELO ROOM

--- gotrip-api/src/models/Rooms.js
+++ gotrip-api/src/models/Rooms.js
@@ -1,1 +1,39 @@
 // Aqui va el modelo Rooms.
+
+const { DataTypes, UUIDV4 } = require("sequelize");
+
+module.exports = (sequelize) => {
+  // TODO Definicion del modelo
+
+  sequelize.define("Rooms", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: UUIDV4,
+    },
+    room: {
+      type: DataTypes.ENUM("simple", "familiar", "group"),
+      defaultValue: "simple",
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      valiate: {
+        min: 0,
+        max: 10000,
+      },
+      kindRoom: {
+        type: DataTypes.ENUM("Standard", "Executive", "Premium", "VIP"),
+        allowNull: false,
+        defaultValue: "VIP"
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    },
+  });
+};
