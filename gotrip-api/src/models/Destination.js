941d78cc711dc92821dce5da0de3f32b81f2dcaa
SH-ur <sdjsm2003@gmail.com>
2023-06-13 13:31:33 -0400

    modelos Destination y Gallery completos

--- gotrip-api/src/models/Destination.js
+++ gotrip-api/src/models/Destination.js
@@ -1,1 +1,39 @@
 // Aqui va el modelo Destination.
+
+const{DataTypes, UUIDV4}=require('sequelize');
+
+module.exports =(sequelize)=>{
+sequelize.define('Destination', {
+id:{
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+},
+country:{
+    type: DataTypes.STRING,
+    allowNull :false,
+    
+},
+state:{
+    type:DataTypes.STRING,
+    allowNull: false,
+    
+},
+city:{
+    type: DataTypes.STRING,
+    validate:{
+        notEmpty: true,
+    }
+},
+moneyType:{
+    type: DataTypes.ENUM("USD", "EUR", "MXN"),
+    allowNull: false,
+},
+status:{
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+}
+}, {timestamps: false})
+}
+
