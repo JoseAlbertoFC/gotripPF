ef1d7951acdd2ad67217efb5af060d8ef88170b6
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-16 18:18:22 -0300

    Ruta getAllRatings lista y funcionando

--- gotrip-api/src/routes/Rating.js
+++ gotrip-api/src/routes/Rating.js
@@ -1,10 +1,11 @@
 const { Router } = require ("express");
 const { postRating } = require("../handlers/RatingHandlers/postRating");
+const { getRatings } = require("../handlers/RatingHandlers/getRatings");
 
 const ratingRoutes = Router();
 
 ratingRoutes.post("/", postRating);
-
+ratingRoutes.get("/", getRatings);
 
 
 module.exports = ratingRoutes;
