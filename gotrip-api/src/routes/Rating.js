4f67f943b543969fce687c2a4f384234d85da0f4
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-16 19:00:32 -0300

    Ruta deleteRating lista

--- gotrip-api/src/routes/Rating.js
+++ gotrip-api/src/routes/Rating.js
@@ -1,12 +1,13 @@
 const { Router } = require ("express");
 const { postRating } = require("../handlers/RatingHandlers/postRating");
 const { getRatings } = require("../handlers/RatingHandlers/getRatings");
+const { deleteRating } = require("../handlers/RatingHandlers/deleteRating");
 
 const ratingRoutes = Router();
 
 ratingRoutes.post("/", postRating);
 ratingRoutes.get("/", getRatings);
-
+ratingRoutes.delete("/:id", deleteRating);
 
 module.exports = ratingRoutes;
 
