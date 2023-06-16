353567897a6360f234f8de65863cd3271e6ed976
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-16 18:02:29 -0300

    Ruta postRating lista y funcionando

--- gotrip-api/src/routes/index.js
+++ gotrip-api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const userRoute = require("./User");
 const bookingRoutes = require("./Booking");
 const payUser = require("./Pay");
+const ratingRoutes = require("./Rating")
 // Importar todos los routers;
 // Ejemplo: const countriesRoutes = require("./Countries");
 
@@ -10,9 +11,10 @@ const router = Router();
 // Ejemplo: router.use("/countries", countriesRoutes);
 
 
-router.use("/user",userRoute)
-router.use("/booking", bookingRoutes)
-router.use("/payment", payUser)
+router.use("/user",userRoute);
+router.use("/booking", bookingRoutes);
+router.use("/payment", payUser);
+router.use("/comments", ratingRoutes);
 
 
 module.exports = router;
