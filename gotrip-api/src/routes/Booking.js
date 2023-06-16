f1b908987a82a2626a9e2e62009efd30d9356855
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-16 01:04:53 -0300

    Ruta GetAllBookigs funcionando

--- gotrip-api/src/routes/Booking.js
+++ gotrip-api/src/routes/Booking.js
@@ -3,7 +3,8 @@ const { postBooking } = require("../handlers/BookingHandlers/postBooking")
 
 const bookingRoutes = Router();
 
-bookingRoutes.post("/", postBooking)
+bookingRoutes.post("/", postBooking);
+bookingRoutes.get("/", postBooking);
 
 module.exports = bookingRoutes;
 
