8335306eb2529f448c32698d0ff7380057074707
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-16 14:40:20 -0300

    Elementos sobreescritos en db restaurados

--- gotrip-api/src/db.js
+++ gotrip-api/src/db.js
@@ -39,29 +39,29 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Hotel, Rooms, Service, Pay, User, Gallery, Rating, Destination, Booking } =
   sequelize.models;
 
-  Hotel.hasMany(Rooms, { as: 'rooms', foreignKey: 'hotelId' });
+  Hotel.hasMany(Rooms, { as: 'rooms', foreignKey: 'roomsId' });
   Rooms.belongsTo(Hotel, { as: 'hotel', foreignKey: 'hotelId' });
   
-  Destination.hasMany(Hotel, { as: 'hotel', foreignKey: 'destinationId' });
+  Destination.hasMany(Hotel, { as: 'hotel', foreignKey: 'hotelId' });
   Hotel.belongsTo(Destination, { as: 'destination', foreignKey: 'destinationId' });
   
-  Hotel.hasMany(Gallery, { as: 'gallery', foreignKey: 'hotelId' });
+  Hotel.hasMany(Gallery, { as: 'gallery', foreignKey: 'galleryId' });
   Gallery.belongsTo(Hotel, { as: 'hotel', foreignKey: 'hotelId' });
   
-  Hotel.hasMany(Rating, { as: 'rating', foreignKey: 'hotelId' });
+  Hotel.hasMany(Rating, { as: 'rating', foreignKey: 'ratingId' });
   Rating.belongsTo(Hotel, { as: 'hotel', foreignKey: 'hotelId' });
   Rating.belongsTo(User, { as: 'user', foreignKey: 'userId' });
   
-  Hotel.hasMany(Booking, { as: 'booking', foreignKey: 'hotelId' });
+  Hotel.hasMany(Booking, { as: 'booking', foreignKey: 'bookingId' });
   Booking.belongsTo(Hotel, { as: 'hotel', foreignKey: 'hotelId' });
   Booking.belongsTo(User, { as: 'user', foreignKey: 'userId' });
   
-  User.hasMany(Pay, { as: 'pay', foreignKey: 'userId' });
+  User.hasMany(Pay, { as: 'pay', foreignKey: 'payId' });
   Pay.belongsTo(User, { as: 'user', foreignKey: 'userId' });
-  Pay.hasOne(Booking, { as: 'booking', foreignKey: 'payId' });
+  Pay.hasOne(Booking, { as: 'booking', foreignKey: 'bookingId' });
   
-  Booking.hasMany(Rooms, { as: 'rooms', foreignKey: 'bookingId' });
-  Booking.hasOne(Pay, { as: 'pay', foreignKey: 'bookingId' });
+  Booking.hasMany(Rooms, { as: 'rooms', foreignKey: 'roomsId' });
+  Booking.hasOne(Pay, { as: 'pay', foreignKey: 'payId' });
   Rooms.belongsTo(Booking, { as: 'booking', foreignKey: 'bookingId' });
 
 Rooms.belongsToMany(Service, {through: "Rooms_Service"});
