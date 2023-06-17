a4d003ee3024d2d42e1f14efb92315fd291b3c7b
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-16 23:03:52 -0300

    Ruta deleteService lista y funcionando

--- gotrip-api/src/routes/Service.js
+++ gotrip-api/src/routes/Service.js
@@ -1,14 +1,12 @@
-const { Router } = require ("express");
-const { postService } = require ("../handlers/ServiceHandlers/postService");
-const { getServices } = require ("../handlers/ServiceHandlers/getService");
+const { Router } = require("express");
+const { postService } = require("../handlers/ServiceHandlers/postService");
+const { getServices } = require("../handlers/ServiceHandlers/getService");
+const { deleteService } = require("../handlers/ServiceHandlers/deleteService");
 
 const serviceRoutes = Router();
 
 serviceRoutes.post("/", postService);
-serviceRoutes.get("/", getServices)
-
-
-
+serviceRoutes.get("/", getServices);
+serviceRoutes.delete("/:id", deleteService)
 
 module.exports = serviceRoutes;
-
