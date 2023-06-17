b219eede204f95baf9c5a027c45ed92c5a29ee89
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-17 00:26:41 -0300

    Ruta updateBooking lista y funcionando

--- gotrip-api/src/routes/Booking.js
+++ gotrip-api/src/routes/Booking.js
@@ -2,7 +2,8 @@ const { Router } = require("express");
 const { postBooking } = require("../handlers/BookingHandlers/postBooking");
 const { getBookings } = require("../handlers/BookingHandlers/getBookings");
 const { getBookingById } = require("../handlers/BookingHandlers/getBookingById");
-const { deleteBooking } = require("../handlers/BookingHandlers/deleteBooking")
+const { deleteBooking } = require("../handlers/BookingHandlers/deleteBooking");
+const { putBooking } = require("../handlers/BookingHandlers/putBooking");
 
 const bookingRoutes = Router();
 
@@ -10,5 +11,6 @@ bookingRoutes.post("/", postBooking);
 bookingRoutes.get("/", getBookings);
 bookingRoutes.get("/:id", getBookingById);
 bookingRoutes.delete("/:id", deleteBooking);
+bookingRoutes.put("/updatebooking/:id", putBooking);
 
 module.exports = bookingRoutes;
