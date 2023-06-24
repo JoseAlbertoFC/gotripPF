46d1d1e008635d6a08c3a23e522fd59297541663
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-06-24 14:47:53 -0300

    Relaciones sobreescritas arregladas

--- gotrip-api/src/db.js
+++ gotrip-api/src/db.js
@@ -45,38 +45,34 @@ const {
   Booking,
 } = sequelize.models;
 
-Hotel.hasMany(Rooms, { as: "rooms", foreignKey: "hotelId" });
-Rooms.belongsTo(Hotel, { as: "hotel", foreignKey: "hotelId" });
-
 Destination.hasMany(Hotel, { as: "hotel", foreignKey: "destinationId" });
-Hotel.belongsTo(Destination, {
-  as: "destination",
-  foreignKey: "destinationId",
-});
 
+Hotel.hasMany(Rooms, { as: "rooms", foreignKey: "hotelId" });
+Hotel.belongsTo(Destination, { as: "destination", foreignKey: "destinationId"});
 Hotel.hasMany(Gallery, { as: "gallery", foreignKey: "hotelId" });
-Gallery.belongsTo(Hotel, { as: "hotel", foreignKey: "hotelId" });
-
 Hotel.hasMany(Rating, { as: "rating", foreignKey: "hotelId" });
+Hotel.hasMany(Booking, { as: 'booking', foreignKey: 'hotelId' });
+
+Rooms.belongsTo(Hotel, { as: "hotel", foreignKey: "hotelId" });
+Rooms.belongsToMany(Service, { through: "Rooms_Service" });
+
+Service.belongsToMany(Rooms, { through: "Rooms_Service" });
+
+Booking.belongsTo(Rooms, { as: 'rooms', foreignKey: 'roomId' });
+Booking.hasOne(Pay, { as: 'pay', foreignKey: 'bookingId' });
+Booking.belongsTo(Hotel, { as: 'hotel', foreignKey: 'hotelId' });
+Booking.belongsTo(User, { as: 'user', foreignKey: 'userId' });
+
 Rating.belongsTo(Hotel, { as: "hotel", foreignKey: "hotelId" });
 Rating.belongsTo(User, { as: "user", foreignKey: "userId" });
 Rating.belongsTo(Rooms, { as: "rooms", foreignKey: "userId" });
 
-Booking.belongsTo(Rooms, { as: 'rooms', foreignKey: 'roomId' });
-Booking.hasOne(Pay, { as: 'pay', foreignKey: 'bookingId' });
-
-
-Hotel.hasMany(Booking, { as: 'booking', foreignKey: 'hotelId' });
-Booking.belongsTo(Hotel, { as: 'hotel', foreignKey: 'hotelId' });
-Booking.belongsTo(User, { as: 'user', foreignKey: 'hotelId' });
-  
 User.hasMany(Pay, { as: 'pay', foreignKey: 'userId' });
+
 Pay.belongsTo(User, { as: 'user', foreignKey: 'userId' });
 Pay.belongsTo(Booking, { as: 'booking', foreignKey: 'bookingId' });
-  
 
-Rooms.belongsToMany(Service, { through: "Rooms_Service" });
-Service.belongsToMany(Rooms, { through: "Rooms_Service" });
+Gallery.belongsTo(Hotel, { as: "hotel", foreignKey: "hotelId" });
 
 module.exports = {
   ...sequelize.models,
