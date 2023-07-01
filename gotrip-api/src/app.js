534ea196b7f4b149e49eda4fe0d8f90a04046738
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-07-01 18:36:25 -0300

    Revert "Configuracion del mal terminada"

--- gotrip-api/src/app.js
+++ gotrip-api/src/app.js
@@ -1,48 +1,47 @@
-require("./db.js");
-const express = require("express");
-
-const morgan = require("morgan");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-
-const routes = require("./routes/index.js");
-
-const session = require("express-session");
-const RedisStore = require("connect-redis").default;
-const { createClient } = require("redis");
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const routes = require('./routes/index.js');
+require('./db.js');
 
 const server = express();
-server.name = "API";
+server.name = 'API';
 
-let redisClient = createClient();
-redisClient.connect().catch(console.error);
-
-let redisStore = new RedisStore({
-  client: redisClient,
-  prefix: "myapp:",
-});
-
-server.use(
-  session({
-    store: redisStore,
-    resave: false,
-    saveUninitialized: false,
-    secret: "keyboard cat",
-  })
-);
-
-server.use(express.urlencoded({ extended: true, limit: "50mb" }));
-server.use(express.json({ limit: "50mb" }));
-server.use(cookieParser());
-server.use(morgan("dev"));
+// server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json());
+// server.use(cookieParser());
+server.use(morgan('dev'));
 server.use(
   cors({
     origin: "*",
   })
-);
+)
+// server.use(cors());
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
+// Configuracion para usar Facebook login 
+server.set('view engine', 'ejs');
 
-server.set("view engine", "ejs");
 
-server.use("/", routes);
+server.use(morgan('dev'));
+server.use('/', routes);
+
+// Error catching endware.
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = server;
