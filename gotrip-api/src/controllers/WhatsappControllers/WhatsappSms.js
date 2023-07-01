53ca38ec98c177549b0c0d2a0d99e39c4b029a3f
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-07-01 11:03:28 -0300

    Ajuste de controlador sms de WhatsApp listo para deploy

--- gotrip-api/src/controllers/WhatsappControllers/WhatsappSms.js
+++ gotrip-api/src/controllers/WhatsappControllers/WhatsappSms.js
@@ -1,15 +1,12 @@
 // Llamamos a las Respuestas de Whatsapp
-const automaticResponses = require('../WhatsappControllers/Response')
+const automaticResponses = require('../WhatsappControllers/Response');
 const twilio = require('twilio');
 const accountSid = process.env.WHATSAPP_CLIENT;
 const authToken = process.env.WHATSAPP_SECRET;
 const client = twilio(accountSid, authToken);
 
-
-
-// Funcion para Enviar Mensajes de whatsapp Automatico.
+// FunciÃ³n para Enviar Mensajes de WhatsApp AutomÃ¡ticos.
 const WhatsappSms = (message, sender) => {
-
     try {
         if (message === 'hola') {
             const mensagge = client.messages.create({
@@ -17,7 +14,7 @@ const WhatsappSms = (message, sender) => {
                 to: sender,
                 body: automaticResponses[0].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
@@ -27,18 +24,17 @@ const WhatsappSms = (message, sender) => {
                 to: sender,
                 body: automaticResponses[1].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
-
         } else if (message === 'disponibilidad') {
             const mensagge = client.messages.create({
                 from: 'whatsapp:+14155238886',
                 to: sender,
                 body: automaticResponses[2].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
@@ -48,7 +44,7 @@ const WhatsappSms = (message, sender) => {
                 to: sender,
                 body: automaticResponses[3].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
@@ -58,18 +54,17 @@ const WhatsappSms = (message, sender) => {
                 to: sender,
                 body: automaticResponses[4].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
-        }
-        else if (message === 'cancelar') {
+        } else if (message === 'cancelar') {
             const mensagge = client.messages.create({
                 from: 'whatsapp:+14155238886',
                 to: sender,
                 body: automaticResponses[5].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
@@ -79,7 +74,7 @@ const WhatsappSms = (message, sender) => {
                 to: sender,
                 body: automaticResponses[6].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
@@ -89,7 +84,7 @@ const WhatsappSms = (message, sender) => {
                 to: sender,
                 body: automaticResponses[7].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
@@ -99,20 +94,19 @@ const WhatsappSms = (message, sender) => {
                 to: sender,
                 body: automaticResponses[8].response
             }).then((mensagge) => {
-                console.log('Respuesta enviada con éxito.');
+                console.log('Respuesta enviada con Ã©xito.');
             }).catch((err) => {
                 console.error('Error al enviar la respuesta:', err);
             });
         }
-        
-        return message
-    } catch (error) {
-        throw new Error  ({ error: error.message })
-        
-    }
 
-}
-    module.exports = WhatsappSms;
+        return message;
+    } catch (error) {
+        throw new Error({ error: error.message });
+    }
+};
+
+module.exports = WhatsappSms;
 
 
 
