d9609d5594abf68e3b6e2312d1fcfed2c36b637e
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-07-01 13:46:37 -0300

    Configuracion lista aparentemente

--- gotrip-api/src/app.js
+++ gotrip-api/src/app.js
@@ -8,9 +8,9 @@ require('./db.js');
 const server = express();
 server.name = 'API';
 
-server.use(express.urlencoded({ extended: true, limit: '50mb' }));
-server.use(express.json({ limit: '50mb' }));
-server.use(cookieParser());
+// server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json());
+// server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(
   cors({
@@ -18,16 +18,16 @@ server.use(
   })
 )
 // server.use(cors());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 // Configuracion para usar Facebook login 
 server.set('view engine', 'ejs');
 
