63cb09e4b784031ead17681ce7a9dc87a6c1b4ba
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-07-05 15:52:27 -0300

    milagritos

--- gotrip-api/src/dal/google-auth.dal.js
+++ gotrip-api/src/dal/google-auth.dal.js
@@ -1,23 +1,23 @@
-const { User } = require('../db');
+const { User } = require("../db");
 
 const googleAuthDal = {
   registerWithGoogle: async (oauthUser) => {
-    console.log(oauthUser)
-    // const isUserExists = await User.findOne({
-    //   email: oauthUser.email,
-    // });
-    // if (isUserExists) {
-    //   const failure = {
-    //     message: 'User already Registered.',
-    //   };
-    //   return { failure };
-    // }
+    console.log(oauthUser);
+    const isUserExists = await User.findOne({
+      where: { email: oauthUser.email },
+    });
+    if (isUserExists) {
+      const failure = {
+        message: "User already Registered.",
+      };
+      return { failure };
+    }
 
     const user = new User({
       name: oauthUser.displayName,
       thirdPartyCreated: true,
-      email: oauthUser.email, 
-      photoUser: null, 
+      email: oauthUser.email,
+      photoUser: null,
       password: "",
       gender: "Not Available",
       address: "Not Available",
@@ -25,7 +25,7 @@ const googleAuthDal = {
       rol: "user",
       postalCode: "Not Available",
       phone: "Not available",
-      phoneCode: 'Not available :)',
+      phoneCode: "Not available :)",
       confirmPassword: "",
       birthday: "Not Available",
       country: "Not available",
@@ -33,12 +33,10 @@ const googleAuthDal = {
     });
     await user.save();
     const success = {
-      message: 'User Registered.',
+      message: "User Registered.",
     };
     return { success };
   },
-
-
 };
 
 module.exports = googleAuthDal;
