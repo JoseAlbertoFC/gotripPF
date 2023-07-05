390a538e6d34346f602e9ef4bfe69ac342bc314f
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-07-05 15:14:19 -0300

    milagrito por favor

--- gotrip-api/src/dal/google-auth.dal.js
+++ gotrip-api/src/dal/google-auth.dal.js
@@ -3,8 +3,7 @@ const User = require('../db');
 const googleAuthDal = {
   registerWithGoogle: async (oauthUser) => {
     const isUserExists = await User.findOne({
-      accountId: oauthUser.id,
-      provider: oauthUser.provider,
+      email: oauthUser.email,
     });
     if (isUserExists) {
       const failure = {
@@ -18,6 +17,18 @@ const googleAuthDal = {
       thirdPartyCreated: true,
       email: oauthUser.emails[0].value, 
       photoUser: oauthUser.photos[0].value, 
+      password: "",
+      gender: "Not Available",
+      address: "Not Available",
+      dniPasaport: "Not available",
+      rol: "user",
+      postalCode: "Not Available",
+      phone: "Not available",
+      phoneCode: 'Not available :)',
+      confirmPassword: "",
+      birthday: "Not Available",
+      country: "Not available",
+      status: true,
     });
     await user.save();
     const success = {
